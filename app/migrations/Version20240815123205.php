3d07c8fcb802e40a281624b0a5fff53ec460f4f4
Marcin Dzierwa <vientvotredouleur@gmail.com>
2024-08-15 14:44:28 +0200

    refactored templates and added doctrine extensions

--- app/migrations/Version20240815123205.php
+++ app/migrations/Version20240815123205.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240602172258 extends AbstractMigration
+final class Version20240815123205 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240602172258 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE tasks CHANGE category_id category_id INT NOT NULL');
+        $this->addSql('ALTER TABLE tasks CHANGE category_id category_id INT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE tasks CHANGE category_id category_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE tasks CHANGE category_id category_id INT NOT NULL');
     }
 }
--- app/migrations/Version20240815123347.php
+++ app/migrations/Version20240815123347.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240602170222 extends AbstractMigration
+final class Version20240815123347 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,14 @@ final class Version20240602170222 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE tasks ADD category_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE tasks ADD CONSTRAINT FK_5058659712469DE2 FOREIGN KEY (category_id) REFERENCES categories (id)');
-        $this->addSql('CREATE INDEX IDX_5058659712469DE2 ON tasks (category_id)');
+        $this->addSql('ALTER TABLE tasks DROP FOREIGN KEY FK_5058659712469DE2');
+        $this->addSql('ALTER TABLE tasks ADD CONSTRAINT FK_5058659712469DE2 FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE SET NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE tasks DROP FOREIGN KEY FK_5058659712469DE2');
-        $this->addSql('DROP INDEX IDX_5058659712469DE2 ON tasks');
-        $this->addSql('ALTER TABLE tasks DROP category_id');
+        $this->addSql('ALTER TABLE tasks ADD CONSTRAINT FK_5058659712469DE2 FOREIGN KEY (category_id) REFERENCES categories (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
     }
 }
